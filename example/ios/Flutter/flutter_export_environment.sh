bd4d4e9bdab06a742bc9c63791a7875c88a89d07
kevinasurjadi <gt_kevin_alexander@hotmail.com>
2023-04-25 21:09:47 +0700

    chore: upgrade flutter version and permission handler

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/kevinalexandersurjadi/fvm/versions/2.5.1"
-export "FLUTTER_APPLICATION_PATH=/Users/kevinalexandersurjadi/Desktop/Personal/Projects/alice/example"
+export "FLUTTER_ROOT=/Users/alex/fvm/versions/2.10.5"
+export "FLUTTER_APPLICATION_PATH=/Users/alex/Desktop/Personal/Espot/Projects/alice/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
